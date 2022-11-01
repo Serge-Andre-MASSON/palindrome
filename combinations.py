7a08ca76dfc1566b70c3aa9daa4b2f948cd7b1aa
Serge <serge.andre.masson@gmail.com>
2022-11-01 12:02:56 +0100

    Update combinations.py

--- combinations.py
+++ combinations.py
@@ -16,37 +16,6 @@ class Combination(list):
 
     def __eq__(self, other: "Combination"):
         return self.product == other.product
-        
+
     def __gt__(self, other: "Combination"):
         return self.product > other.product
-
-
-
-def get_layers(layer: list[Combination], min_: int):
-    while layer:
-        next_layer = []
-        last_products = [c.product for c in layer]
-        for combination in layer:
-            for next_combination in next_combinations(combination, min_):
-                n = next_combination.product
-                if n in last_products:
-                    pass
-                else:
-                    last_products.append(n)
-                    next_layer.append(next_combination)
-        layer = next_layer
-        if layer:
-            yield layer
-
-
-def next_combinations(c: Combination, min_: int):
-    l = len(c)
-    for i, el in enumerate(c):
-        tmp = c.copy()
-        if i < l - 1:
-            if el > c[i + 1]:
-                tmp[i] = max(el - 1, min_)
-                yield Combination(tmp)
-        else:
-            tmp[i] = max(el - 1, min_)
-            yield Combination(tmp)
